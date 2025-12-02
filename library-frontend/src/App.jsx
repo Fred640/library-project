f992de41c690610fa0ba10bdd856c157e67a6c5a
Fred <sapronovfedor760@gmail.com>
2025-12-02 21:09:04 +0300

    API Initial commit

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,5 +1,33 @@
-const App = () => {
-  return(
-    <div></div>
-  )
+import axios from "axios"
+import React, { Component } from 'react';
+class App extends React.Component{
+  state = {details:[],}
+
+  componentDidMount(){
+    let data;
+    axios.get("http://127.0.0.1:8000")
+    .then(res => {
+      data = res.data
+      this.setState({
+        details:data
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+  render() {
+    return (
+      <div>
+        <p>asd</p>
+        {this.state.details.map((output, id) => (
+        <div key={id}>
+          <p>{output.title}</p>
+          <p>{output.author}</p>
+        </div>))}
+      </div>
+    )
+  }
 }
+
+export default App
--- library/library/settings.py
+++ library/library/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,16 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.AllowAny'
-    ]
-}
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],}
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'library.urls'
 
